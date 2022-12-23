fb34ee9d37c5630186c75eb2d6118cde1108c745
N1k0L1n <nikolinandoni80@gmail.com>
2022-12-23 16:11:05 +0100

    end of today

---
+++ src/app/features/catalog/Catalog.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import {Product} from "../../models/product"
+import ProductList from "./ProductList";
+
+
+
+
+export default function Catalog() {
+      
+
+  const [products, setProducts] = useState <Product[]>([]);
+
+
+  useEffect(()=>{
+    fetch('https://localhost:7086/api/Products', {})
+    .then (response => response.json())
+    .then(data=>setProducts(data))
+  },[])
+  
+    return (
+
+        <>
+            <ProductList products={products}/>
+            
+        </>
+    )
+}
---
+++ src/app/features/catalog/ProductCard.tsx
@@ -0,0 +1,47 @@
+import {  Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import CardHeader from "@mui/material/CardHeader";
+import { Product } from "../../models/product";
+
+
+interface Props {
+    product:Product;
+}
+
+export default function ProductCard({product}:Props){
+    return(
+        <Card>
+          <CardHeader avatar={
+                <Avatar sx={{bgcolor:"secondary.main"}}>
+                    {product.name.charAt(0).toUpperCase()}
+                </Avatar>
+          }
+
+          title={product.name}
+          titleTypographyProps={{
+              sx : {fontWeight:"bold", color:'primary.main'}
+          }}
+
+          />
+          <CardMedia
+          sx = {{height:140, backgroundSize:"contain", bgcolor:"primary.light"}}
+          image= 'http://picsum.photos/200'
+          title={product.name}
+           />
+
+
+            <CardContent>
+                <Typography gutterBottom color='secondary' variant="h5">
+                     ${(product.price/100).toFixed(2)}
+                 </Typography>
+              <Typography variant="body2" color="text.secondary">
+                    {product.brand}/{product.type}
+                </Typography>
+            </CardContent>
+          <CardActions>
+              <Button size="small">Add to cart</Button>
+              <Button size="small">View</Button>
+        </CardActions>
+      </Card>
+    )
+}
